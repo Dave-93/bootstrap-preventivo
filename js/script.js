8acd76dd01cb16a8c06da228e938a9fe648f3153
Dave-93 <difrancesco.davide93@gmail.com>
2025-01-14 16:07:22 +0100

    feat: price calculation and coupon check

---
+++ js/script.js
@@ -0,0 +1,77 @@
+const inputName = document.querySelector("#input-name");
+const inputSurname = document.querySelector("#input-surname");
+const inputEmail = document.querySelector("#input-email");
+const inputSelect = document.querySelector("#input-select");
+const inputTextarea = document.querySelector("#input-textarea");
+const inputPromoCode = document.querySelector("#input-promo-code");
+const inputCheck = document.querySelector("#input-check");
+
+const submitPreventive = document.querySelector("#submit-preventive");
+const resultPreventive = document.querySelector("#result-preventive");
+const resultCoupon = document.querySelector("#result-coupon");
+
+const couponList = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+
+submitPreventive.addEventListener("click", function(event){
+    event.preventDefault();
+    
+// CALCOLO DEGLI SCONTI 
+    const couponBackEnd = ((10 * 20.5) * (25 / 100));
+    const couponFrontEnd = ((10 * 15.3) * (25 / 100));
+    const couponProject = ((10 * 33.6) * (25 / 100));
+//
+
+    let couponActive = checkCoupon(inputPromoCode);//TODO prende il risultato restituito dalla funzione
+    console.log(couponActive)
+
+// CALCOLO PREZZO e CONTROLLO PER APPLICARE LO SCONTO
+    if((inputSelect.value === "1") && (couponActive === "Ok")){//aggiungere il calcolo dello sconto coupon conFUNZIONE???
+        let backEnd = (10 * 20.5) - couponBackEnd;
+        resultPreventive.innerHTML = `€ ${backEnd.toFixed(2).replace(".",",")}`; 
+    }else if(inputSelect.value === "1"){
+        let backEnd = (10 * 20.5);
+        resultPreventive.innerHTML = `€ ${backEnd.toFixed(2).replace(".",",")}`;
+    }else if((inputSelect.value === "2") && (couponActive === "Ok")){
+        let frontEnd = (10 * 15.3) - couponFrontEnd;
+        resultPreventive.innerHTML = `€ ${frontEnd.toFixed(2).replace(".",",")}`; 
+    }else if(inputSelect.value === "2"){
+        let frontEnd = (10 * 15.3);
+        resultPreventive.innerHTML = `€ ${frontEnd.toFixed(2).replace(".",",")}`; 
+    }else if((inputSelect.value === "3") && (couponActive === "Ok")){
+        let project = (10 * 33.6) - couponProject;
+        resultPreventive.innerHTML = `€ ${project.toFixed(2).replace(".",",")}`; 
+    }else if(inputSelect.value === "3"){
+        let project = (10 * 33.6);
+        resultPreventive.innerHTML = `€ ${project.toFixed(2).replace(".",",")}`;
+    }
+//
+})
+
+// CONTROLLO SUL COUPON
+function checkCoupon(input){
+    let couponActive;
+    if(!(input.value)){
+        resultCoupon.innerHTML = "Coupon assente!"
+        resultCoupon.classList.remove("text-success");
+        resultCoupon.classList.remove("text-danger");
+        resultCoupon.classList.add("text-info");
+        couponActive = "No";//assegna il valore alla variabile
+        return couponActive;//restituisce quel valore
+    }else if(couponList.includes(input.value)){
+        resultCoupon.innerHTML = "Coupon valido!"
+        resultCoupon.classList.remove("text-danger");
+        resultCoupon.classList.remove("text-info");
+        resultCoupon.classList.add("text-success");
+        couponActive = "Ok";//assegna il valore alla variabile
+        return couponActive;//restituisce quel valore
+    }else{
+        resultCoupon.innerHTML = "Coupon non valido!"
+        resultCoupon.classList.remove("text-success");
+        resultCoupon.classList.remove("text-info");
+        resultCoupon.classList.add("text-danger");
+        couponActive = "No";//assegna il valore alla variabile
+        return couponActive;//restituisce quel valore
+    }
+}
+//
